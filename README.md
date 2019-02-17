1ecc27e3823a390a267ae06fa5784357f0c776ab
A. Elmeniawy <ameniawy18@gmail.com>
2019-02-17 19:10:44 +0200

    added module creating script

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Advanced-Data-Structures-And-Algorithms
+
+* A1: Disjoint Sets & Persistent Data Structures
+* A2: Segment Trees
+* A3: Skip Lists 
---
+++ create_module.sh
@@ -0,0 +1,20 @@
+
+echo "Enter module number."
+read MODULE
+mkdir A$MODULE
+cd A$MODULE
+mkdir Assignment
+mkdir Lab
+cp ../Template.java ./Assignment/AssignmentA.java
+cp ../Template.java ./Assignment/AssignmentB.java
+cp ../Template.java ./Lab/A.java
+cp ../Template.java ./Lab/B.java
+sed -i 's/Template/AssignmentA/g' ./Assignment/AssignmentA.java
+sed -i 's/Template/AssignmentB/g' ./Assignment/AssignmentB.java
+sed -i 's/Template/A/g' ./Lab/A.java
+sed -i 's/Template/B/g' ./Lab/B.java
+
+sed -i 's/XXXX/Assignment/g' ./Assignment/AssignmentA.java
+sed -i 's/XXXX/Assignment/g' ./Assignment/AssignmentB.java
+sed -i 's/XXXX/Lab/g' ./Lab/A.java
+sed -i 's/XXXX/Lab/g' ./Lab/B.java
